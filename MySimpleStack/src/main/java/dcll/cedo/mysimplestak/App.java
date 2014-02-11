2fb7e642dbbb7dfe8a7fe90758244c272e5c00b5
21003451 <21003451@208-02.mig.snap.fsi>
2014-02-11 09:11:22 +0100

    Mon premier commit du TP2

---
+++ MySimpleStack/src/main/java/dcll/cedo/mysimplestak/App.java
@@ -0,0 +1,13 @@
+package dcll.cedo.mysimplestak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
