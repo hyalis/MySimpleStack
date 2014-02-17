d677671333a8e645980769837f2afb7f476adccf
unknown <21003451@204-07.mig.snap.fsi>
2014-02-17 14:14:56 +0100

    Debut le 17/02/14

--- MySimpleStack/src/main/java/dcll/cedo/mysimplestak/testUneStack.java
+++
@@ -1,7 +0,0 @@
-package dcll.cedo.mysimplestak;
-
-import junit.framework.TestCase;
-
-public class testUneStack extends TestCase {
-
-}
---
+++ MySimpleStack/src/test/java/dcll/cedo/mysimplestak/UneStackTest.java
@@ -0,0 +1,42 @@
+package dcll.cedo.mysimplestak;
+
+import junit.framework.TestCase;
+
+public class UneStackTest extends TestCase {
+	
+	UneStack maStackDeTest;
+	
+	public UneStackTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		this.maStackDeTest = new UneStack();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testIsEmpty() {
+		assert(this.maStackDeTest.isEmpty());
+	}
+
+	public void testGetSize() {
+		assert(this.maStackDeTest.getSize()==0);
+	}
+
+	public void testPush() {
+		this.maStackDeTest.push("Hello");
+		assert(this.maStackDeTest.peek().equals("Hello"));
+	}
+
+	public void testPeek() {
+		assert(this.maStackDeTest.peek().equals("Hello"));
+	}
+
+	public void testPop() {	
+		assert(this.maStackDeTest.pop().equals("Hello"));
+	}
+
+}
